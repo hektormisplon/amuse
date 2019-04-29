f1b857c624ec54dfe9368b77c5586c9c20d96b72
unknown <hektor.misplon@gmail.com>
2019-04-29 10:52:15 +0200

    project structure

---
+++ src/server/api/v1/database/schemas/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+import Blog from './blog.schema';
+import Category from './category.schema';
+import Post from './post.schema';
+import User from './user.schema';
+
+export {
+    Blog,
+    Category,
+    Post,
+    User,
+};
