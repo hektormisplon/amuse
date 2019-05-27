1987506f114b889d685cc4a836b3633e417ac229
unknown <hektor.misplon@gmail.com>
2019-05-27 10:12:13 +0200

    styling

--- app/src/native-client/components/StyledText.js
+++ app/src/native-client/components/StyledText.js
@@ -7,15 +7,15 @@ export class Text extends React.Component {
   }
 }
 
+// 32
 export class Title extends React.Component {
   render() {
-    return <NativeText {... this.props} style={[this.props.style, {fontFamily: 'VremenaGrotesk-medium', fontSize: 28}]}></NativeText>
+    return <NativeText {... this.props} style={[this.props.style, {fontFamily: 'VremenaGrotesk-medium', fontSize: 32}]}></NativeText>
   }
 }
 
-export class TextWhite extends React.Component {
+export class SubTitle extends React.Component {
   render() {
-    return <NativeText {... this.props} style={[this.props.style, {fontFamily: 'HKGrotesk-light', fontSize: 20}]}></NativeText>
+    return <NativeText {... this.props} style={[this.props.style, {fontFamily: 'HKGrotesk-medium', fontSize: 20}]}></NativeText>
   }
 }
-
--- app/src/native-client/screens/ProfileScreen.js
+++ app/src/native-client/screens/ProfileScreen.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { MapView } from 'expo';
 import {} from '@expo/vector-icons/Feather'
 
 
@@ -8,6 +7,7 @@ import {} from '@expo/vector-icons/Feather'
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
     title: 'Profile',
+      header: null,
   };
 
   render() {
