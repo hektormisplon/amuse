3633cd1119da8ab458076e7b0d95e90832007100
hektormisplon <hektor.misplon@gmail.com>
2019-06-11 17:18:04 +0200

    client signup

---
+++ app/src/client/src/app/pages/auth/SignUp.jsx
@@ -0,0 +1,154 @@
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+import PersonAdd from '@material-ui/icons/PersonAdd';
+import axios from 'axios';
+/*
+Import extenal libraries
+*/
+import React, { Component } from 'react';
+
+/*
+Components
+*/
+
+/*
+Styling
+*/
+const styles = theme => ({
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
+
+class Signup extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  handleInputChange = (event, value) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+  handleSubmit = event => {
+    console.log('sign-up handleSubmit, username: ');
+    console.log(this.state.username);
+    event.preventDefault();
+
+    console.log(this.state);
+
+    axios
+      .post('/api/v1/users/', {
+        username: this.state.username,
+        email: this.state.email,
+        localProvider: {
+          password: this.state.password
+        }
+      })
+      .then(response => {
+        console.log(response);
+        if (!response.data.errmsg) {
+          console.log('Signed up');
+          this.props.history.push('/login');
+        } else {
+          console.warn('Username taken');
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <PersonAdd />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                autoComplete="email"
+                onChange={this.handleInputChange}
+                value={this.state.email}
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.handleInputChange}
+                name="password"
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={this.handleSubmit}
+            >
+              Sign up
+            </Button>
+          </form>
+        </Paper>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withStyles(styles)(Signup);
--- app/src/client/src/app/pages/login/LoginPage.jsx
+++ app/src/client/src/app/pages/login/LoginPage.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import SignUp from '../auth/SignUp';
 
 class LoginPage extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Sign in</h1>
+        {/* <h1>Sign in</h1>
         <form className={'sign-in-form'}>
           <label htmlFor="email">Email</label>
           <input type="email" />
           <label htmlFor="password">Password</label>
           <input type="password" />
           <button type="submit">Sign In</button>
-        </form>
+        </form> */}
+        <SignUp />
       </React.Fragment>
     );
   }
