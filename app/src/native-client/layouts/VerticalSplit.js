b5d0c493cb6ac2871174a60d1c73b9537c041bb1
hektormisplon <hektor.misplon@gmail.com>
2019-06-12 18:49:59 +0200

    split layout props

--- app/src/native-client/layouts/VerticalSplit.js
+++ app/src/native-client/layouts/VerticalSplit.js
@@ -1,25 +1,44 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Colors } from '../styles'
 
 const VerticalSplit = props => {
+  const { top, bottom, colored, noPadding } = props
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.top}>{props.top}</View>
-      <View style={styles.bottom}>{props.bottom}</View>
+    <View style={{ flex: 1, padding: !noPadding ? 30 : 0 }}>
+      <View
+        style={[
+          styles.top,
+          {
+            backgroundColor:
+              colored === 'top' ? Colors.primaryBrand.light : null
+          }
+        ]}
+      >
+        {top}
+      </View>
+      <View
+        style={[
+          styles.bottom,
+          {
+            backgroundColor:
+              colored === 'bottom' ? Colors.primaryBrand.light : null
+          }
+        ]}
+      >
+        {bottom}
+      </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   top: {
     flex: 0.62,
-    justifyContent: "center"
-    // color: Colors.primaryBrand.light,
-    // backgroundColor: Colors.primaryBrand.light
+    justifyContent: 'center'
   },
   bottom: {
     flex: 0.38
-    // color: Colors.primaryBrand.light
   }
-});
-export default VerticalSplit;
+})
+export default VerticalSplit
