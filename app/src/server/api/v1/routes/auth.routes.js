b3ca60bdd9035ae8a95b51d906a39ee9ac763576
hektormisplon <hektor.misplon@gmail.com>
2019-06-14 00:05:19 +0200

    swagger documentin

--- app/src/server/api/v1/routes/auth.routes.js
+++ app/src/server/api/v1/routes/auth.routes.js
@@ -4,6 +4,19 @@ import { AuthController } from '../controller';
 const authController = new AuthController();
 
 const initializeEndpoints = (parentRouter, authService) => {
+    /**
+     * @swagger
+     * /api/v1/login/local:
+     *   post:
+     *     tags:
+     *       - Login
+     *     description: Login using JWT local strategy
+     *     produces:
+     *       - application/json
+     *     responses:
+     *       200:
+     *         description: A JWT Local strategy login object
+     */
     parentRouter.post('/login/local', (req, res, next) => authController.loginLocal(authService, req, res, next));
 };
 
