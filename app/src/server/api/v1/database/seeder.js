3ecba31b5b1840379ff38483b8f7bc052b1aba15
hektormisplon <hektor.misplon@gmail.com>
2019-06-14 00:05:09 +0200

    linting & prettier

--- app/src/server/api/v1/database/seeder.js
+++ app/src/server/api/v1/database/seeder.js
@@ -3,13 +3,14 @@ Import the external libraries:
 - faker
 */
 import faker from 'faker';
-
 /*
 Import the internal libraries:
 - logger
 */
 import { logger } from '../../../utilities';
-import { Blog, Category, Post, Tour, Museum, User, Club, Badge } from './schemas';
+import {
+    Blog, Category, Museum, Post, Tour, User,
+} from './schemas';
 
 class Seeder {
     constructor() {
@@ -38,9 +39,12 @@ class Seeder {
 
             logger.log({ level: 'info', message: `Blog created with id: ${newblog.id}!` });
         } catch (err) {
-            logger.log({ level: 'info', message: `An error occurred when creating a blog: ${err}!` });
+            logger.log({
+                level: 'info',
+                message: `An error occurred when creating a blog: ${err}!`,
+            });
         }
-    }
+    };
 
     categoryCreate = async (name, description) => {
         const categoryDetail = {
@@ -56,9 +60,12 @@ class Seeder {
 
             logger.log({ level: 'info', message: `Category created with id: ${newCategory.id}!` });
         } catch (err) {
-            logger.log({ level: 'info', message: `An error occurred when creating a category: ${err}!` });
+            logger.log({
+                level: 'info',
+                message: `An error occurred when creating a category: ${err}!`,
+            });
         }
-    }
+    };
 
     postCreate = async (title, synopsis, body) => {
         const postDetail = {
@@ -75,9 +82,12 @@ class Seeder {
 
             logger.log({ level: 'info', message: `Post created with id: ${newPost.id}!` });
         } catch (err) {
-            logger.log({ level: 'info', message: `An error occurred when creating a post: ${err}!` });
+            logger.log({
+                level: 'info',
+                message: `An error occurred when creating a post: ${err}!`,
+            });
         }
-    }
+    };
 
     userCreate = async (email, password) => {
         const userDetail = {
@@ -94,11 +104,13 @@ class Seeder {
 
             logger.log({ level: 'info', message: `User created with id: ${newUser.id}!` });
         } catch (err) {
-            logger.log({ level: 'info', message: `An error occurred when creating a user: ${err}!` });
+            logger.log({
+                level: 'info',
+                message: `An error occurred when creating a user: ${err}!`,
+            });
         }
-    }
+    };
 
-    
     tourCreate = async (title) => {
         const tourDetail = {
             title,
@@ -111,10 +123,12 @@ class Seeder {
             this.tours.push(newTour);
             logger.log({ level: 'info', message: `Post created with id: ${newTour.id}!` });
         } catch (err) {
-            logger.log({ level: 'info', message: `An error occurred when creating a post: ${err}!` });
+            logger.log({
+                level: 'info',
+                message: `An error occurred when creating a post: ${err}!`,
+            });
         }
-    }
-
+    };
 
     museumCreate = async (shortName, name, coords) => {
         const museumDetail = {
@@ -129,16 +143,19 @@ class Seeder {
             this.musea.push(newMuseum);
 
             logger.log({ level: 'info', message: `Museum created with id: ${newMuseum.id}!` });
-        } catch(err) {
-            logger.log({ level: 'info', message: `An error occurred when creating a museum: ${err}!` });
+        } catch (err) {
+            logger.log({
+                level: 'info',
+                message: `An error occurred when creating a museum: ${err}!`,
+            });
         }
-    }
+    };
 
     createBlogs = async () => {
         await Promise.all([
             (async () => this.blogCreate(faker.lorem.sentence(), faker.lorem.paragraph()))(),
         ]);
-    }
+    };
 
     createCategories = async () => {
         await Promise.all([
@@ -147,36 +164,72 @@ class Seeder {
             (async () => this.categoryCreate(faker.lorem.word(), faker.lorem.sentence()))(),
             (async () => this.categoryCreate(faker.lorem.word(), faker.lorem.sentence()))(),
         ]);
-    }
+    };
 
     createPosts = async () => {
         await Promise.all([
-            (async () => this.postCreate(faker.lorem.sentence(), faker.lorem.paragraph(), `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`))(),
-            (async () => this.postCreate(faker.lorem.sentence(), faker.lorem.paragraph(), `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`))(),
-            (async () => this.postCreate(faker.lorem.sentence(), faker.lorem.paragraph(), `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`))(),
-            (async () => this.postCreate(faker.lorem.sentence(), faker.lorem.paragraph(), `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`))(),
-            (async () => this.postCreate(faker.lorem.sentence(), faker.lorem.paragraph(), `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`))(),
-            (async () => this.postCreate(faker.lorem.sentence(), faker.lorem.paragraph(), `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`))(),
-            (async () => this.postCreate(faker.lorem.sentence(), faker.lorem.paragraph(), `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`))(),
+            (async () => this.postCreate(
+                faker.lorem.sentence(),
+                faker.lorem.paragraph(),
+                `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`,
+            ))(),
+            (async () => this.postCreate(
+                faker.lorem.sentence(),
+                faker.lorem.paragraph(),
+                `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`,
+            ))(),
+            (async () => this.postCreate(
+                faker.lorem.sentence(),
+                faker.lorem.paragraph(),
+                `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`,
+            ))(),
+            (async () => this.postCreate(
+                faker.lorem.sentence(),
+                faker.lorem.paragraph(),
+                `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`,
+            ))(),
+            (async () => this.postCreate(
+                faker.lorem.sentence(),
+                faker.lorem.paragraph(),
+                `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`,
+            ))(),
+            (async () => this.postCreate(
+                faker.lorem.sentence(),
+                faker.lorem.paragraph(),
+                `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`,
+            ))(),
+            (async () => this.postCreate(
+                faker.lorem.sentence(),
+                faker.lorem.paragraph(),
+                `<p>${faker.lorem.paragraphs(10, '</p></p>')}</p>`,
+            ))(),
         ]);
-    }
+    };
 
     createTours = async () => {
         await Promise.all([
-            (async () => this.tourCreate(faker.lorem.sentence())),
-            (async () => this.tourCreate(faker.lorem.sentence())),
-            (async () => this.tourCreate(faker.lorem.sentence())),
-        ])
-    }
+            async () => this.tourCreate(faker.lorem.sentence()),
+            async () => this.tourCreate(faker.lorem.sentence()),
+            async () => this.tourCreate(faker.lorem.sentence()),
+        ]);
+    };
 
     createMusea = async () => {
         await Promise.all([
-            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [{lng: 0, lat: 0}]))(),
-            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [{lng: 0, lat: 0}]))(),
-            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [{lng: 0, lat: 0}]))(),
-            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [{lng: 0, lat: 0}]))(),
+            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [
+                { lng: 0, lat: 0 },
+            ]))(),
+            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [
+                { lng: 0, lat: 0 },
+            ]))(),
+            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [
+                { lng: 0, lat: 0 },
+            ]))(),
+            (async () => this.museumCreate(faker.lorem.sentence(), faker.lorem.sentence(), [
+                { lng: 0, lat: 0 },
+            ]))(),
         ]);
-    }
+    };
 
     createUsers = async () => {
         await Promise.all([
@@ -188,16 +241,15 @@ class Seeder {
             (async () => this.userCreate(faker.internet.email(), 'wicked4u'))(),
             (async () => this.userCreate(faker.internet.email(), 'wicked4u'))(),
         ]);
-    }
-    
-    
+    };
+
     getRandomCategory = () => {
         let category = null;
         if (this.categories && this.categories.length > 0) {
             category = this.categories[Math.round(Math.random() * (this.categories.length - 1))];
         }
         return category;
-    }
+    };
 
     getRandomPosts = () => {
         let cPosts = null;
@@ -209,46 +261,57 @@ class Seeder {
             }
         }
         return cPosts;
-    }
+    };
 
     seed = async () => {
-        this.categories = await Category.estimatedDocumentCount().exec().then(async (count) => {
-            if (count === 0) {
-                await this.createCategories();
-            }
-            return Category.find().exec();
-        });
-        this.posts = await Post.estimatedDocumentCount().exec().then(async (count) => {
-            if (count === 0) {
-                await this.createPosts();
-            }
-            return Post.find().exec();
-        });
-        this.blogs = await Blog.estimatedDocumentCount().exec().then(async (count) => {
-            if (count === 0) {
-                await this.createBlogs();
-            }
-            return Blog.find().exec();
-        });
-        this.tours = await Tour.estimatedDocumentCount().exec().then(async (count) => {
-            if(count === 0) {
-                await this.createTours();
-            }
-            return Tour.find().exec();
-        });
-        this.musea = await Museum.estimatedDocumentCount().exec().then(async (count) => {
-            if (count === 0) {
-                await this.createMusea();
-            }
-            return Museum.find().exec();
-        });
-        this.users = await User.estimatedDocumentCount().exec().then(async (count) => {
-            if (count === 0) {
-                await this.createUsers();
-            }
-            return User.find().exec();
-        });
-        
-    }
+        this.categories = await Category.estimatedDocumentCount()
+            .exec()
+            .then(async (count) => {
+                if (count === 0) {
+                    await this.createCategories();
+                }
+                return Category.find().exec();
+            });
+        this.posts = await Post.estimatedDocumentCount()
+            .exec()
+            .then(async (count) => {
+                if (count === 0) {
+                    await this.createPosts();
+                }
+                return Post.find().exec();
+            });
+        this.blogs = await Blog.estimatedDocumentCount()
+            .exec()
+            .then(async (count) => {
+                if (count === 0) {
+                    await this.createBlogs();
+                }
+                return Blog.find().exec();
+            });
+        this.tours = await Tour.estimatedDocumentCount()
+            .exec()
+            .then(async (count) => {
+                if (count === 0) {
+                    await this.createTours();
+                }
+                return Tour.find().exec();
+            });
+        this.musea = await Museum.estimatedDocumentCount()
+            .exec()
+            .then(async (count) => {
+                if (count === 0) {
+                    await this.createMusea();
+                }
+                return Museum.find().exec();
+            });
+        this.users = await User.estimatedDocumentCount()
+            .exec()
+            .then(async (count) => {
+                if (count === 0) {
+                    await this.createUsers();
+                }
+                return User.find().exec();
+            });
+    };
 }
 export default Seeder;
