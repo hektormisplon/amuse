17e78abbb89a46b68bbda595d8a1f4ddd5cdd916
hektormisplon <hektor.misplon@gmail.com>
2019-06-16 17:26:56 +0200

    transitions

--- app/src/native-client/styles/index.js
+++ app/src/native-client/styles/index.js
@@ -2,5 +2,6 @@ import * as Badges from './badges'
 import Colors from './colors'
 import Dimensions from './dimensions'
 import MapStyle from './mapStyle'
+import * as ScreenTransitions from './screenTransitions'
 
-export { Colors, Dimensions, MapStyle, Badges }
+export { Colors, Dimensions, MapStyle, Badges, ScreenTransitions }
