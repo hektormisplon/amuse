179d4c33aac538ad0f3eef0d5ca7f90f6af3c5af
hektormisplon <hektor.misplon@gmail.com>
2019-06-16 21:38:31 +0200

    auth validation upon app load

--- app/src/native-client/screens/AuthLoadingScreen.js
+++ app/src/native-client/screens/AuthLoadingScreen.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react'
 import { ActivityIndicator, Button, StatusBar, View } from 'react-native'
 import { DeviceStorage } from '../services'
 
+import { Colors } from '../styles'
+
+
+/**
+ * Loading screen & authentication redirect
+ */
 export default class AuthLoadingScreen extends Component {
-  constructor(props) {
-    super(props)
-    this._bootstrapAsync()
+
+  componentDidMount() {
+    this.checkUserAuthentication()
   }
 
-  _bootstrapAsync = async () => {
-    const jwtToken = DeviceStorage.load('jwtToken')
+  checkUserAuthentication = async () => {
+    let jwtToken = await DeviceStorage.load('jwtToken')
     this.props.navigation.navigate(jwtToken ? 'Main' : 'Auth')
   }
 
   render() {
     return (
-      <View>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: Colors.primaryBrand.light }}>
         <ActivityIndicator />
         <StatusBar barStyle="default" />
         <Button
           title="Skip authentication check"
           onPress={() => {
-            this.props.navigation.navigate('Main')
+            this.props.navigation.navigate('Auth')
           }}
         />
       </View>
